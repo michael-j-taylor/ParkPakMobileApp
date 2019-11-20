1722e05568b8a78d807a9d86558afd426b76cbd5
michael-j-taylor <michaeljtaylor15@gmail.com>
2019-11-20 16:46:06 -0600

    checkbox for payment activity

--- app/src/main/java/com/example/parkpalmobileapp/CreateNewUser.kt
+++ app/src/main/java/com/example/parkpalmobileapp/CreateNewUser.kt
@@ -1,9 +1,11 @@
 package com.example.parkpalmobileapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
 
 class CreateNewUser : AppCompatActivity() {
 
@@ -11,11 +13,35 @@ class CreateNewUser : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_user)
 
+        var autopayment : Boolean = false
+
         val create_add_user = findViewById<Button>(R.id.create_add_user)
+        val autopayment_checkbox = findViewById<CheckBox>(R.id.autopayment_checkbox)
+        val button_skip = findViewById<Button>(R.id.payment_skip2)
+
+        autopayment_checkbox.setOnClickListener(View.OnClickListener {
+            if (autopayment_checkbox.isChecked) {
+                autopayment = true
+            }
+            if (!autopayment_checkbox.isChecked) {
+                autopayment = false
+            }
+        })
+
+        button_skip.setOnClickListener {
+            val intent = Intent(this, MainMenuActivity :: class.java)
+            startActivity(intent)
+        }
 
         create_add_user.setOnClickListener {
-            val add_user_intent = Intent(this, PaymentActivity :: class.java)
-            startActivity(add_user_intent)
+            if (autopayment) {
+                val add_user_intent = Intent(this, PaymentActivity :: class.java)
+                startActivity(add_user_intent)
+            }
+            else {
+                val add_user_intent = Intent(this, MainMenuActivity :: class.java)
+                startActivity(add_user_intent)
+            }
         }
     }
 }
